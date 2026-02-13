14f9d8b93ce81e055b33371d34610ab74f51335f
alebossale89-afk <alebossale89@gmail.com>
2026-02-13 22:02:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Siiiiii amoreeee";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -43,11 +43,11 @@ function resizeYesButton() {
 function generateMessage(noCount) {
   const messages = [
     "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "comme no?",
+    "daiiiiii amore",
+    "perchè no? T_T",
+    "amore miooo eddaiiii",
+    "amoreeeeeeeee",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
